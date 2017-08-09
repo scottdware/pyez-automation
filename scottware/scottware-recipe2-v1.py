3f6dfdc671d71dc25d47ee0898bcb07d133e8eaa
Scott Ware <scottdware@gmail.com>
2017-08-09 14:45:01 -0400

    Modified script to include a main function

--- scottware/scottware-recipe2-v1.py
+++ scottware/scottware-recipe2-v1.py
@@ -5,25 +5,29 @@ import csv
 
 USER = "admin"
 PW = "starwars"
-CSV_FILE = "config-values.csv"
 CONFIG_FILE = "config.txt"
 
-with open(CSV_FILE) as f:
-    csvfile = csv.DictReader(f)
+def config_multi_devices(csv_file='config-data.csv', config_template=CONFIG_FILE):
+    with open(csv_file) as f:
+        csvfile = csv.DictReader(f)
 
-    for row in csvfile:
-        fw = row['firewall']
-        values = {
-            'dns_server': row['dns_server'],
-            'ntp_server': row['ntp_server'],
-            'snmp_location': row['snmp_location'],
-            'snmp_contact': row['snmp_contact'],
-            'snmp_community': row['snmp_community'],
-            'snmp_trap_recvr': row['snmp_trap_recvr']
-        }
+        for row in csvfile:
+            firewall = row['firewall']
+            values = {
+                'dns_server': row['dns_server'],
+                'ntp_server': row['ntp_server'],
+                'snmp_location': row['snmp_location'],
+                'snmp_contact': row['snmp_contact'],
+                'snmp_community': row['snmp_community'],
+                'snmp_trap_recvr': row['snmp_trap_recvr']
+            }
 
-        dev = Device(host=fw, user=USER, password=PW).open()
-        with Config(dev) as cu:
-            cu.load(template_path=CONFIG_FILE, template_vars=values, format='set', merge=True)
-            cu.commit(timeout=30)
-        dev.close()
+            dev = Device(host=firewall, user=USER, password=PW).open()
+            with Config(dev) as cu:
+                cu.load(template_path=CONFIG_FILE, template_vars=values, format='set', merge=True)
+                cu.commit(timeout=30)
+                print("Committing the configuration on device: {}".format(firewall))
+            dev.close()
+
+if __name__ == "__main__":
+    config_multi_devices()
